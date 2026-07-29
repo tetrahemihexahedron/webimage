c6f2d097e369d6f8c5bc87307661c9ad4f717678
Sunny Fawcett <95454232+tetrahemihexahedron@users.noreply.github.com>
2026-07-29 11:51:33 -0600

    Add function to fetch exiftool output

--- exif/exif.go
+++ exif/exif.go
@@ -1,5 +1,12 @@
 package exif
 
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os/exec"
+)
+
 type ImageMetadata struct {
 	FileName    string
 	FileType    string
@@ -11,4 +18,33 @@ type ImageMetadata struct {
 
 func FetchMetadata(path string) ([]ImageMetadata, []error) {
 	return []ImageMetadata{}, nil
+}
+
+type exiftoolOutput struct {
+	FileName    string `json:"FileName"`
+	FileType    string `json:"FileType"`
+	Title       string `json:"Title"`
+	Description string `json:"Description"`
+	Width       uint   `json:"ImageWidth"`
+	Height      uint   `json:"ImageHeight"`
+	Error       string `json:"Error"`
+}
+
+func fetchExiftoolOutput(path string) ([]exiftoolOutput, error) {
+	cmd := exec.Command("exiftool", "-json", path)
+
+	out, err := cmd.Output()
+
+	if len(out) == 0 {
+		if exitErr, ok := errors.AsType[*exec.ExitError](err); ok {
+			return nil, fmt.Errorf("running exiftool on %q: %s", path, exitErr.Stderr)
+		}
+		return nil, fmt.Errorf("running exiftool on %q: %w", path, err)
+	}
+
+	var structuredOutput []exiftoolOutput
+	if err := json.Unmarshal(out, &structuredOutput); err != nil {
+		return structuredOutput, fmt.Errorf("unmarshalling exiftool output for %q: %w", path, err)
+	}
+	return structuredOutput, nil
 }
