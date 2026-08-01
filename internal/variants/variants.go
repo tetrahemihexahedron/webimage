72e91616cc05d4a65ee2fdf7f8b678b15cae77ff
Sunny Fawcett <95454232+tetrahemihexahedron@users.noreply.github.com>
2026-08-01 16:21:27 -0600

    Check that source is nonempty, and add loop over Specs, using a stub for generateVariant

--- internal/variants/variants.go
+++ internal/variants/variants.go
@@ -1,10 +1,34 @@
 package variants
 
+import (
+	"errors"
+	"fmt"
+)
+
 type Spec struct {
 	OutPath string
 	Width   uint
 }
 
 func Generate(source string, specs []Spec) error {
+	if source == "" {
+		return errors.New("source file path cannot be empty")
+	}
+
+	var errs []error
+	for _, spec := range specs {
+		if err := generateVariant(source, spec); err != nil {
+			errs = append(errs, fmt.Errorf(
+				"generating %q with width %d: %w",
+				spec.OutPath,
+				spec.Width,
+				err,
+			))
+		}
+	}
+	return errors.Join(errs...)
+}
+
+func generateVariant(source string, spec Spec) error {
 	return nil
 }
