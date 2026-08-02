0b49c2b9357a581012256e4e09669ad29f8fb329
Sunny Fawcett <95454232+tetrahemihexahedron@users.noreply.github.com>
2026-08-02 08:09:51 -0600

    Skip non-jpegs. Add function that hashes the image file

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"io"
 	"log"
+	"os"
+	"path/filepath"
 	"tetrahemihexahedron/webimage/internal/config"
 	"tetrahemihexahedron/webimage/internal/exif"
 )
@@ -20,4 +25,34 @@ func main() {
 	for _, err = range errs {
 		log.Printf("Metadata error: %v", err)
 	}
+
+	for _, metadata := range allMetadata {
+		if metadata.FileType != "JPEG" {
+			log.Printf("Skipping file %s: file type is %s, not JPEG", metadata.FileName, metadata.FileType)
+			continue
+		}
+
+		inPath := filepath.Join(config.InDir, metadata.FileName)
+		hash, err := hashFile(inPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		shortHash := hash[:10]
+		log.Printf("short hash for %s is %s", inPath, shortHash)
+	}
+}
+
+func hashFile(filename string) (string, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	h := sha256.New()
+	if _, err := io.Copy(h, f); err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(h.Sum(nil)), nil
 }
