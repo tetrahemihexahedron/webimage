39eafa321de64a85e510da73e535d42bf0042ebc
Sunny Fawcett <95454232+tetrahemihexahedron@users.noreply.github.com>
2026-08-02 07:38:47 -0600

    Load metadata and log errors

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"tetrahemihexahedron/webimage/internal/config"
+	"tetrahemihexahedron/webimage/internal/exif"
 )
 
 func main() {
@@ -12,4 +13,11 @@ func main() {
 	}
 
 	log.Printf("Loaded config: %+v", config)
+
+	allMetadata, errs := exif.FetchMetadata(config.InDir)
+
+	log.Printf("Fetched metadata for %d files. %d error(s).", len(allMetadata), len(errs))
+	for _, err = range errs {
+		log.Printf("Metadata error: %v", err)
+	}
 }
