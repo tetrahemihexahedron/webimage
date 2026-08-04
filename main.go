004ebd015bac155c3dddfd9bc455abf66359b0d8
Sunny Fawcett <95454232+tetrahemihexahedron@users.noreply.github.com>
2026-08-04 10:38:05 -0600

    Move the processing that was happening in main to a 'processor' package

--- main.go
+++ main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
-	"io"
 	"log"
-	"os"
-	"path/filepath"
-	"strings"
 	"tetrahemihexahedron/webimage/internal/config"
-	"tetrahemihexahedron/webimage/internal/exif"
-	"tetrahemihexahedron/webimage/internal/image"
+	"tetrahemihexahedron/webimage/internal/processor"
 )
 
 func main() {
@@ -21,55 +14,5 @@ func main() {
 
 	log.Printf("Loaded config: %+v", config)
 
-	allMetadata, errs := exif.FetchMetadata(config.InDir)
-
-	log.Printf("Fetched metadata for %d files. %d error(s).", len(allMetadata), len(errs))
-	for _, err = range errs {
-		log.Printf("Metadata error: %v", err)
-	}
-
-	for _, metadata := range allMetadata {
-		if metadata.FileType != image.FormatJPEG {
-			log.Printf("Skipping file %s: file type is %s, not JPEG", metadata.FileName, metadata.FileType)
-			continue
-		}
-
-		inPath := filepath.Join(config.InDir, metadata.FileName)
-		hash, err := hashFile(inPath)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		shortHash := hash[:10]
-		imageDir := imageDir(metadata.FileName, shortHash)
-
-		image := image.Processed{
-			Hash:     hash,
-			ImageDir: imageDir,
-			Metadata: metadata,
-		}
-
-		log.Printf("image is %+v", image)
-	}
-}
-
-func hashFile(filename string) (string, error) {
-	f, err := os.Open(filename)
-	if err != nil {
-		return "", err
-	}
-	defer f.Close()
-
-	h := sha256.New()
-	if _, err := io.Copy(h, f); err != nil {
-		return "", err
-	}
-
-	return hex.EncodeToString(h.Sum(nil)), nil
-}
-
-func imageDir(filename string, suffix string) string {
-	base := strings.TrimSuffix(filename, filepath.Ext(filename))
-
-	return base + "-" + suffix
+	processor.ProcessDir(config)
 }
